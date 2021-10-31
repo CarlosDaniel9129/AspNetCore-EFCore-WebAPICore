f1fafd7c9f4d4098f7a57022567ff29083cbf111
David-Jesus <carlosdaniel9129@gmail.com>
2021-10-31 17:38:49 -0300

    commit 4

---
+++ EFCore_WebAPICore/Controllers/ClienteController.cs
@@ -0,0 +1,44 @@
+﻿using EFCore_WebAPICore.Data;
+using EFCore_WebAPICore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore_WebAPICore.Controllers
+{
+    [Route("api/clientes")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private ClienteContext _context { get; set; } = new ClienteContext();
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Cliente>>> Get()
+        {
+            var clientes = await _context.Clientes.ToListAsync();
+            return clientes;
+        }
+
+        [HttpPost]
+        [Route("inserir")]
+        public async Task<ActionResult<Cliente>> Post([FromBody] Cliente model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Clientes.Add(model);
+                await _context.SaveChangesAsync();
+                return model;
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+    }
+}
